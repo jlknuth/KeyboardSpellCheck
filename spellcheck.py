a2ad2e29a944be6f3bed74f1c27a4ba59342366d
Joseph Knuth <joseph.knuth@numerica.us>
2014-04-10 11:40:13 -0600

    Add support for sublime text 2

--- spellcheck.py
+++ spellcheck.py
@@ -4,9 +4,21 @@ import sys
 import os
 sys.path.append(os.path.join(os.path.dirname(__file__), "pyenchant"))
 import enchant
-import urllib.request
-import urllib.parse
 import re
+
+if sublime.version() < '3000':
+    # we are on ST2 and Python 2.X
+    _ST3 = False
+    from urllib2 import Request
+    from urllib2 import urlopen
+    from urllib2 import quote
+else:
+    _ST3 = True
+    from urllib.request import Request
+    from urllib.request import urlopen
+    from urllib.parse import quote
+
+
 
 
 class SpellCheckCommand(sublime_plugin.TextCommand):
@@ -36,11 +48,11 @@ class SpellCheckCommand(sublime_plugin.TextCommand):
         if gfix is not None:
             if gfix in self.suggestions:
                 self.suggestions.remove(gfix)
-            self.index_suggestions = ['{}: '.format(ix+1)+s for ix, s in enumerate(self.suggestions)]
+            self.index_suggestions = ['{0}: '.format(ix+1)+s for ix, s in enumerate(self.suggestions)]
             self.suggestions = [gfix] + self.suggestions
-            self.index_suggestions = ['{}[G]: '.format(0)+gfix] + self.index_suggestions
+            self.index_suggestions = ['{0}[G]: '.format(0)+gfix] + self.index_suggestions
         else:
-            self.index_suggestions = ['{}: '.format(ix)+s for ix, s in enumerate(self.suggestions)]
+            self.index_suggestions = ['{0}: '.format(ix)+s for ix, s in enumerate(self.suggestions)]
 
         if (not self.dictionary.check(phrase)) or (gfix is not None):
             self.view.window().show_quick_panel(self.index_suggestions,
@@ -53,7 +65,10 @@ class SpellCheckCommand(sublime_plugin.TextCommand):
     def on_done(self, index):
         if (index == -1):
             self.view.sel().clear()
-            self.view.sel().add(sublime.Region(self.pos.a, self.pos.b))
+            if not _ST3:
+                self.view.sel().add(sublime.Region(long(self.pos.a), long(self.pos.b)))
+            else:
+                self.view.sel().add(sublime.Region(self.pos.a, self.pos.b))
             return
         self.view.run_command("insert_my_text", {"args": {'text':self.suggestions[index],
                               'posa':self.pos.a, 'posb':self.pos.b}})
@@ -61,8 +76,9 @@ class SpellCheckCommand(sublime_plugin.TextCommand):
     def correct(self, text):
         # grab html
         try:
-            html = self.get_page('http://www.google.com/search?q=' + urllib.parse.quote(text))
+            html = self.get_page('http://www.google.com/search?q=' + quote(text))
         except:
+            print('KeyboardSpellCheck: Could not contact Google')
             return None
 
         # pull pieces out
@@ -84,8 +100,8 @@ class SpellCheckCommand(sublime_plugin.TextCommand):
         # user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
         user_agent = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.116 Safari/537.36'
         headers = {'User-Agent':user_agent,}
-        req = urllib.request.Request(url, None, headers)
-        page = urllib.request.urlopen(req)
+        req = Request(url, None, headers)
+        page = urlopen(req)
         html = str(page.read())
         page.close()
         return html
@@ -95,6 +111,9 @@ class InsertMyText(sublime_plugin.TextCommand):
   def run(self, edit, args):
     self.view.replace(edit, self.view.sel()[0], args['text'])
     self.view.sel().clear()
-    self.view.sel().add(sublime.Region(args['posa'], args['posb']))
+    if not _ST3:
+        self.view.sel().add(sublime.Region(long(args['posa']), long(args['posb'])))
+    else:
+        self.view.sel().add(sublime.Region(args['posa'], args['posb']))
 
 
